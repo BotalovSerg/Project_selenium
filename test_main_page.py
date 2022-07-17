c81f84d011e87c6b853ebed6cbb80f2952369b6f
Serg <89090168690@mail.ru>
2022-07-17 12:22:18 +0500

    создал pytest.ini для регистрации маркеров в test_main_page.py объединил два теста в один класс

--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,29 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+
+    def test_guest_can_go_to_login_page(self, browser):
+        """
+        тестовый гость может перейти на страницу входа
+        :param browser:
+        :return:
+        """
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     browser.delete_all_cookies()
     link = "http://selenium1py.pythonanywhere.com/"
@@ -16,21 +39,6 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     cart_page.checkout_there_are_no_items_in_the_cart()
     cart_page.checking_that_the_product_was_not_added_to_the_cart()
     cart_page.pending_element_disappeared()
-
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser, browser.current_url)
-#     login_page.should_be_login_page()
-#
-#
-# def test_guest_should_see_login_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
 
 # def test_guest_should_see_login_url(browser):
 #     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
